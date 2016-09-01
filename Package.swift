16ab90efcea8912d7ce193374855f95c642eecae
futbalguy <kyle.rokita@gmail.com>
2016-08-31 20:55:50 -0700

    Updated for Fluent 0.10

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "FluentMySQL",
     dependencies: [
    		.Package(url: "https://github.com/vapor/mysql.git", majorVersion: 0, minor: 4),
-   		.Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 9),
+   		.Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 10),
     ]
 )
